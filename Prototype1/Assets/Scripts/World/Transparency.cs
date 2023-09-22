4f1f486eaa780798bcfbac10decc32e3b80d9a96
Josh Bonovich <joshua.bonovich@tkdtrio.com>
2023-09-22 15:51:25 -0500

    bugs fixes

--- Prototype1/Assets/Scripts/World/Transparency.cs
+++ Prototype1/Assets/Scripts/World/Transparency.cs
@@ -4,41 +4,46 @@ using UnityEngine;
 
 public class Transparency : MonoBehaviour
 {
-    Material mat;
+    Material[] mats;
     [SerializeField]
     float fadeSpeed = 2.0f;
     [SerializeField]
     float fadeAmount = 0.4f;
-    float originalOpacity;
+    float[] originalOpacities;
     private bool fade = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        mat = GetComponent<Renderer>().material;
-        originalOpacity = mat.color.a;
+        mats = GetComponent<Renderer>().materials;
+        originalOpacities = new float[mats.Length];
+        for (int i = 0; i < mats.Length; i++)
+            originalOpacities[i] = mats[i].color.a;
         fade = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Color currentColor = mat.color;
-        Color smoothColor;
-        if (fade)
+        for (int i = 0; i < mats.Length; i++)
         {
-            //Debug.Log("Applying fade");
-            smoothColor = new Color(currentColor.r, currentColor.g, currentColor.b, 
-                Mathf.Lerp(currentColor.a, fadeAmount, fadeSpeed * Time.deltaTime));
+            Color currentColor = mats[i].color;
+            Color smoothColor;
+            if (fade)
+            {
+                //Debug.Log("Applying fade");
+                smoothColor = new Color(currentColor.r, currentColor.g, currentColor.b,
+                    Mathf.Lerp(currentColor.a, fadeAmount, fadeSpeed * Time.deltaTime));
 
+            }
+            else
+            {
+                //Debug.Log("Unapplying fade");
+                smoothColor = new Color(currentColor.r, currentColor.g, currentColor.b,
+                    Mathf.Lerp(currentColor.a, originalOpacities[i], fadeSpeed * Time.deltaTime));
+            }
+            mats[i].color = smoothColor;
         }
-        else
-        {
-            //Debug.Log("Unapplying fade");
-            smoothColor = new Color(currentColor.r, currentColor.g, currentColor.b, 
-                Mathf.Lerp(currentColor.a, originalOpacity, fadeSpeed * Time.deltaTime));
-        }
-        mat.color = smoothColor;
     }
 
     public void DoFade(bool fade)
