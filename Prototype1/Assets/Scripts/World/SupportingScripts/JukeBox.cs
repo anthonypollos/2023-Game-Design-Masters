2005ae2f0cfbe83cd2de4aebddc09372a33717cb
a-chapman2 <achapman2@mail.bradley.edu>
2023-10-17 16:03:53 -0500

    Jukebox Changes and Placeholder Main Menu Music
    Returned the Jukebox to its previous function, with some compromises: now will run chosen audio clips through an audio source on the given object. Any changes to settings in the jukebox element will be applied to the AudioSource while playing where possible. Also, menu music now.

--- Prototype1/Assets/Scripts/World/SupportingScripts/JukeBox.cs
+++ Prototype1/Assets/Scripts/World/SupportingScripts/JukeBox.cs
@@ -10,19 +10,19 @@ public class JukeBox
     [Serializable]
     public struct SoundTrack
     {
-        //public AudioClip sound;
-        public AudioSource source;
-        // [Tooltip("The audio mixer group this sound should output to")] public AudioMixerGroup outputGroup;
-        //public float volume;
-        //[Tooltip("Does the sound play from the object or not")] public bool isLocalized;
-        // [Tooltip("How the AudioSource attenuates over distance")] public AudioRolloffMode rolloffMode;
-       // [Tooltip("The distance a sound starts attenuating at/becomes audible")] public float minDistance;
-        // [Tooltip("The distance a sound stops attenuating at/becomes inaudible")] public float maxDistance;
+        public AudioClip sound;
+        [Tooltip("The audio mixer group this sound should output to")] public AudioMixerGroup outputGroup;
+        public float volume;
+        [Tooltip("Does the sound play from the object or not")] public bool isLocalized;
+        [Tooltip("How the AudioSource attenuates over distance")] public AudioRolloffMode rolloffMode;
+        [Tooltip("The distance a sound starts attenuating at/becomes audible")] public float minDistance;
+        [Tooltip("The distance a sound stops attenuating at/becomes inaudible")] public float maxDistance;
 
     }
 
     [SerializeField] private SoundTrack[] tracks;
     [SerializeField] private Transform transform;
+    [SerializeField] private AudioSource source;
 
     public void SetTransform(Transform transform)
     {
@@ -34,7 +34,7 @@ public class JukeBox
     public void PlaySound(int idx)
     {
  
-        if(idx>tracks.Length-1 || tracks[idx].source == null)
+        if(idx>tracks.Length-1 || source == null)
         {
             Debug.LogError("Error: That audio clip is not set");
             return;
@@ -42,9 +42,24 @@ public class JukeBox
         else
         {
             SoundTrack track = tracks[idx];
-            //Vector3 location = track.isLocalized ? transform.position : Camera.main.transform.position;
+            source.clip = track.sound;
+            source.outputAudioMixerGroup = track.outputGroup;
+            source.volume = track.volume;
+            if (track.isLocalized)
+            {
+                source.spread = 360;
+            }
+            else
+            {
+                source.spread = 0;
+            }
+            source.rolloffMode = track.rolloffMode;
+            source.minDistance = track.minDistance;
+            source.maxDistance = track.maxDistance;
+
+            Vector3 location = track.isLocalized ? transform.position : Camera.main.transform.position;
             //AudioSource.PlayClipAtPoint(track.sound, location, track.volume);
-            track.source.Play();
+            source.Play();
             Debug.Log("Should play");
         }
     }
