841c67101c4386650e455853538d2645edf17932
Wolfcl0ck <16748747+Wolfcl0ck@users.noreply.github.com>
2023-10-25 15:17:11 -0500

    player blood

--- Prototype1/Assets/Scripts/Player/PlayerHealth.cs
+++ Prototype1/Assets/Scripts/Player/PlayerHealth.cs
@@ -8,6 +8,7 @@ public class PlayerHealth : MonoBehaviour, IDamageable
 {
     [SerializeField] JukeBox jukebox;
     [SerializeField] int health = 100;
+    [SerializeField] GameObject bloodParticle;
     int maxHealth;
     IsoPlayerController pc;
     Slider hpBar;
@@ -30,6 +31,14 @@ public class PlayerHealth : MonoBehaviour, IDamageable
     }
     public void TakeDamage(int dmg)
     {
+        //If there is a blood particle, create it.
+        if (bloodParticle != null)
+        {
+            //create the particle
+            GameObject vfxobj = Instantiate(bloodParticle, gameObject.transform.position, Quaternion.identity);
+            //destroy the particle
+            Destroy(vfxobj, 4);
+        }
         jukebox.PlaySound(0);
         health -= dmg;
         hpBar.value = (float)health / (float)maxHealth;
