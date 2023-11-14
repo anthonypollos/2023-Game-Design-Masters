fc94cee440b4fd39a23e056e41e3901092000611
jbthelt <jbarthelt@mail.bradley.edu>
2023-11-14 11:30:29 -0600

    Revert "Import sounds, melee enemy pulled sound"
    This reverts commit 3c83de588af84548bb294a34ed088a6cf62b2ebd.

--- Prototype1/Assets/Scripts/Enemy/Enemy_Melee/MeleeEnemyInteractions.cs
+++ Prototype1/Assets/Scripts/Enemy/Enemy_Melee/MeleeEnemyInteractions.cs
@@ -14,13 +14,7 @@ public class MeleeEnemyInteractions : EnemyInteractionBehaviorTemplate
     [SerializeField]
     [Tooltip("Stun time when taking damage")]
     float stunTime = 0.5f;
-    [SerializeField] private JukeBox jukebox;
-
-    private void Awake()
-    {
-        jukebox.SetTransform(transform);
-    }
-    void Start()
+     void Start()
     {
         lassoed = false;
         stunned = false;
@@ -67,7 +61,6 @@ public class MeleeEnemyInteractions : EnemyInteractionBehaviorTemplate
     {
         base.Pulled();
         launched = true;
-        jukebox.PlaySound(0);
         //lassoed = false;
         hasCollided = false;
         //brain.an.SetBool("Lassoed", false);
