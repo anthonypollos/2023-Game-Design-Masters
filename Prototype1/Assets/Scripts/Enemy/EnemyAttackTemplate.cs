24971f949821760fe93e7e2d7d417e5e3a02589f
Josh Bonovich <joshua.bonovich@tkdtrio.com>
2023-11-16 16:11:55 -0600

    Wind Up and Start Ups

--- Prototype1/Assets/Scripts/Enemy/EnemyAttackTemplate.cs
+++ Prototype1/Assets/Scripts/Enemy/EnemyAttackTemplate.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public abstract class EnemyAttackTemplate : MonoBehaviour
 {
+    [Header("Starting attack data")]
     [SerializeField] 
     [Tooltip("Range at which the enemy can start attacking")]
     public float maxAttackRange;
@@ -15,9 +16,25 @@ public abstract class EnemyAttackTemplate : MonoBehaviour
     public float attackSpeed;
     [HideInInspector]
     public EnemyBrain brain;
+    [Header("Attack timing information")]
+    [SerializeField] protected float[] attackWindUpSeconds;
+    [SerializeField] protected float[] attackSeconds;
+    [SerializeField] protected float[] attackWindDownSeconds;
+    [Header("Attack frame data")]
+    [SerializeField] protected int[] attackFrames;
+    [SerializeField] protected int[] windUpFrames;
+    [SerializeField] protected int[] windDownFrames;
+    [SerializeField] protected int attackFramesPerSecond;
+    
+
+    protected float timeTest;
     public abstract void Attack();
 
     protected float count = 0;
+
+    protected float animationTimer = float.MinValue;
+
+    protected float currentWaitingTime = float.MaxValue;
 
     protected virtual void UpdateCounter()
     {
@@ -31,6 +48,89 @@ public abstract class EnemyAttackTemplate : MonoBehaviour
                 count -= Time.deltaTime / 2;
             count = Mathf.Clamp(count, 0, attackSpeed);
         }
+        if(brain.state == EnemyStates.ATTACKING )
+        {
+            animationTimer += Time.deltaTime;
+            if(animationTimer >= currentWaitingTime)
+            {
+                animationTimer = float.MinValue;
+                //ToDo add animation stuff here
+            }
+        }
         
+    }
+
+    public void WindUpTrigger(int attack)
+    {
+        brain.an.SetFloat("AttackMod", 1);
+        attack = attack - 1;
+        if (attack < 0 && attack >= attackWindUpSeconds.Length)
+        {
+            Debug.LogError("Attack value for WindUp invalid");
+            return;
+        }
+        currentWaitingTime = attackWindUpSeconds[attack];
+        if(animationTimer<0)
+            animationTimer = 0;
+    }
+
+    public void WindDownTrigger(int attack)
+    {
+        brain.an.SetFloat("AttackMod", 1);
+        attack = attack - 1;
+        if (attack < 0 && attack >= attackWindDownSeconds.Length)
+        {
+            Debug.LogError("Attack value for WindDown invalid");
+            return;
+        }
+        currentWaitingTime = attackWindDownSeconds[attack];
+        if(animationTimer<0)
+            animationTimer = 0;
+    }
+
+    public void AttackTrigger(int attack)
+    {
+        attack = attack - 1;
+        if (attack < 0 && attack >= attackSeconds.Length)
+        {
+            Debug.LogError("Attack value for SetAttackSpeed invalid");
+            return;
+        }
+        brain.an.SetFloat("AttackMod", (float)attackFrames[attack] / (attackFramesPerSecond * attackSeconds[attack]));
+    }
+
+
+
+    public void SetWindUpSpeed(int attack)
+    {
+        attack = attack - 1;
+        if (attack<0 && attack>=attackWindUpSeconds.Length)
+        {
+            Debug.LogError("Attack value for SetWindUpSpeed invalid");
+            return;
+        }
+        brain.an.SetFloat("AttackMod", (float)windUpFrames[attack] / (attackFramesPerSecond * attackWindUpSeconds[attack]));
+    }
+
+    public void SetAttackSpeed(int attack)
+    {
+        attack = attack - 1;
+        if (attack < 0 && attack >= attackSeconds.Length)
+        {
+            Debug.LogError("Attack value for SetAttackSpeed invalid");
+            return;
+        }
+        brain.an.SetFloat("AttackMod", (float)attackFrames[attack] / (attackFramesPerSecond * attackSeconds[attack]));
+    }
+
+    public void SetWindDownSpeed(int attack)
+    {
+        attack = attack - 1;
+        if (attack < 0 && attack >= attackWindDownSeconds.Length)
+        {
+            Debug.LogError("Attack value for SetWindDownSpeed invalid");
+            return;
+        }
+        brain.an.SetFloat("AttackMod", (float)windDownFrames[attack] / (attackFramesPerSecond * attackWindDownSeconds[attack]));
     }
 }
