42dbd68c6720e1ac3a6e3697d2a27435a4d0ca63
Josh Bonovich <joshua.bonovich@tkdtrio.com>
2023-11-20 14:08:21 -0600

    CommandUpdate

--- Prototype1/Assets/Scripts/Helpers/DevConsole/Commands/CommandScripts/CompleteMissionCommand.cs
+++ Prototype1/Assets/Scripts/Helpers/DevConsole/Commands/CommandScripts/CompleteMissionCommand.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-[CreateAssetMenu(fileName = "New GodMode Command", menuName = "Commands/CompleteMissionCommand")]
+[CreateAssetMenu(fileName = "New CompleteMission Command", menuName = "Commands/CompleteMissionCommand")]
 public class CompleteMissionCommand : ConsoleCommand
 {
     public override (bool, string) ActivateCommand(string[] args)
     {
         int arg;
         if (!int.TryParse(args[0], out arg))
+        {
+            return (false, "");
+        }
+        if(arg <= 0)
         {
             return (false, "");
         }
