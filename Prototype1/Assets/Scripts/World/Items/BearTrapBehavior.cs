fafdb6147730fb7dafc0eb06738abd70f337664e
jbthelt <jbarthelt@mail.bradley.edu>
2023-11-29 16:44:05 -0600

    Beartrap placeholder sound

--- Prototype1/Assets/Scripts/World/Items/BearTrapBehavior.cs
+++ Prototype1/Assets/Scripts/World/Items/BearTrapBehavior.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class BearTrapBehavior : MonoBehaviour
 {
     [SerializeField] int dmg = 20;
+
+    [SerializeField] private JukeBox jukebox;
+
+    private void Awake()
+    {
+        jukebox.SetTransform(transform);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,6 +44,7 @@ public class BearTrapBehavior : MonoBehaviour
         {
             other.gameObject.GetComponent<EnemyInteractionBehaviorTemplate>().Stun(5);
             temp.TakeDamage(dmg);
+            jukebox.PlaySound(0);
         }
     }
 
