854ea6ceee9dd892d7b608329a4eac8e48537b3a
avery1255 <amacke@mail.bradley.edu>
2023-11-29 12:03:03 -0600

    jukebox fixes, player and enemies updated

--- Prototype1/Assets/Scripts/World/SupportingScripts/JukeBox.cs
+++ Prototype1/Assets/Scripts/World/SupportingScripts/JukeBox.cs
@@ -11,10 +11,10 @@ public class JukeBox
     public struct SoundTrack
     {
         public AudioClip sound;
+        [Tooltip("Master audio mixer")] public AudioMixerGroup masterOutputGroup;   // can't figure out a way to get this via code unfortunately
         [Tooltip("The audio mixer group this sound should output to")] public AudioMixerGroup outputGroup;
-        // Don't need the above, can use mixerParam enum below, just need to be sure to add new parameters if we ever create more mixers
-        public enum mixerParameter { AmbientVol, SFXVol, EnemyVol, MasterVol, MusicVol, PlayerVol, VoiceVol, WorldVol }
-        [Tooltip("The volume paramater of desired audio mixer group")] public mixerParameter outputMixerGroup;
+        public enum mixerParameter { AmbientVol, SFXVol, MusicVol, VoiceVol }
+        [Tooltip("The volume paramater of desired audio mixer group")] public mixerParameter outputVolParameter;
 
         [Tooltip("On a scale of 0.0 to 1.0")] public float volume;
         [Tooltip("Does the sound play from the object or not")] public bool isLocalized;
@@ -49,8 +49,15 @@ public class JukeBox
             Vector3 location = track.isLocalized ? transform.position : Camera.main.transform.position;
             if (track.sound != null)
             {
-                bool value = track.outputGroup.audioMixer.GetFloat(track.outputMixerGroup.ToString(), out float adjustVol);
-                adjustVol = Mathf.Pow(10f, adjustVol / 20) * track.volume; 
+                // get volume of desired output mixer
+                bool value = track.outputGroup.audioMixer.GetFloat(track.outputVolParameter.ToString(), out float trackVol);
+
+                // get volume of master volume mixer; NEED THIS to make sounds adjust with master volume
+                bool masterValue = track.masterOutputGroup.audioMixer.GetFloat("MasterVol", out float masterVol);
+                masterVol = Mathf.Pow(10f, masterVol / 20);
+
+                // adjusts volume of clip; output mixer vol * track vol * master mixer vol
+                float adjustVol = Mathf.Pow(10f, trackVol / 20) * track.volume * masterVol;
 
                 AudioSource.PlayClipAtPoint(track.sound, location, adjustVol /*track.volume*/);
                 //source.Play();
