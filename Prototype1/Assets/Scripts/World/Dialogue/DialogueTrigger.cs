f6234dbbe21482518961efaef2d236b869bc5894
Josh Bonovich <joshua.bonovich@tkdtrio.com>
2023-12-07 14:49:22 -0600

    Update DialogueTrigger.cs

--- Prototype1/Assets/Scripts/World/Dialogue/DialogueTrigger.cs
+++ Prototype1/Assets/Scripts/World/Dialogue/DialogueTrigger.cs
@@ -8,10 +8,25 @@ using Ink.Runtime;
 public class DialogueTrigger: InteractableBehaviorTemplate
 {
     [SerializeField] TextAsset dialogText;
+    [SerializeField] TextAsset dialogText2;
+    bool interacted = false;
 
     public override bool Interact()
     {
-        DialogueManager.instance.EnterDialogMode(dialogText);
+        if (interacted)
+        {
+            if (dialogText2 != null)
+                DialogueManager.instance.EnterDialogMode(dialogText2);
+            else
+                Debug.Log("Second Dialogue");
+        }
+        else
+        {
+            if (dialogText != null)
+                DialogueManager.instance.EnterDialogMode(dialogText);
+            else
+                Debug.Log("First Dialogue");
+        }
         return false;
     }
 }
