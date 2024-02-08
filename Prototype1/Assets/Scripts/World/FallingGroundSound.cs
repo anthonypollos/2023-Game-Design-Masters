b425826204364fc20b2541b69b74e25f0954f7d1
jbthelt <jbarthelt@mail.bradley.edu>
2024-02-08 15:44:02 -0600

    New sounds added, implemented falling ground sound

---
+++ Prototype1/Assets/Scripts/World/FallingGroundSound.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingGroundSound : MonoBehaviour
+{
+    
+    public AudioSource asource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        asource = GetComponent<AudioSource>();
+        //asource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlaySound()
+    {
+        asource.Play();
+    }
+
+}
