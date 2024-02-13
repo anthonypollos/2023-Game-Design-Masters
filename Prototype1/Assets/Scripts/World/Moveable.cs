1566e807022222079a6601236530b55285508d6c
a-chapman2 <achapman2@mail.bradley.edu>
2024-02-13 15:35:49 -0600

    Build Fix
    commented out superfluous using statement that was preventing builds

--- Prototype1/Assets/Scripts/World/Moveable.cs
+++ Prototype1/Assets/Scripts/World/Moveable.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Unity.VisualScripting;
-using UnityEditor.UIElements;
+//using UnityEditor.UIElements;
 //using UnityEditor.SceneTemplate;
 using UnityEngine;
 using static Unity.Burst.Intrinsics.X86.Avx;
