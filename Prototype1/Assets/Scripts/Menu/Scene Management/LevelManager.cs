5f4331b56196eaf7aaa66a8241d3f501339c1e1d
avery1255 <amacke@mail.bradley.edu>
2024-02-26 14:08:10 -0600

    hub level select

--- Prototype1/Assets/Scripts/Menu/Scene Management/LevelManager.cs
+++ Prototype1/Assets/Scripts/Menu/Scene Management/LevelManager.cs
@@ -14,6 +14,8 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private GameObject loadScreen;
     [SerializeField] private Image progressBar;
 
+    private float fillTarget;
+
     private string sceneToLoad;
 
     // Start is called before the first frame update
@@ -28,22 +30,54 @@ public class LevelManager : MonoBehaviour
             Destroy(gameObject);
     }
 
+    private void Update()
+    {
+        progressBar.fillAmount = Mathf.MoveTowards(progressBar.fillAmount, fillTarget, 1 * Time.deltaTime);
+    }
+
     /// <summary>
     /// 
     /// </summary>
     /// <param name="sceneName">Scene to Load</param>
     public async void LoadScene(string sceneName)
     {
-        var scene = SceneManager.LoadSceneAsync(sceneName);
-        scene.allowSceneActivation = false;
+        fillTarget = 0;
+        progressBar.fillAmount = 0;
+
         loadScreen.SetActive(true);
 
-        do {
-            await Task.Delay(100);        // delete this
-            progressBar.fillAmount = scene.progress + 0.1f;
-        } while (scene.progress < 0.9f);
+        await Task.Delay(2);
 
-        scene.allowSceneActivation = true;
+        var scene = SceneManager.LoadSceneAsync(sceneName);
+        scene.allowSceneActivation = false;
+
+        while(!scene.isDone)
+        {
+            fillTarget = scene.progress + 0.1f;
+
+            if (scene.progress >= 0.9f)
+            {
+                await Task.Delay(1);
+                //fillTarget = 1.0f;
+                scene.allowSceneActivation = true;
+            }
+        }
+
+        await Task.Delay(1);
+
         loadScreen.SetActive(false);
+
+        //do {
+        //    await Task.Delay(100);
+        //    fillTarget = scene.progress;// + 0.1f;
+        //} while (scene.progress < 0.9f);
+
+        //await Task.Delay(1000);
+
+        //scene.allowSceneActivation = true;
+
+        //await Task.Delay(10);
+
+        //loadScreen.SetActive(false);
     }
 }
