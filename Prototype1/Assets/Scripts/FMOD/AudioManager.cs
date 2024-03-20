29c549e5638fb30f753df078dd512281e051fc8d
jbthelt <jbarthelt@mail.bradley.edu>
2024-03-20 11:26:13 -0500

    Setting up fmod audio manager

---
+++ Prototype1/Assets/Scripts/FMOD/AudioManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class AudioManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
