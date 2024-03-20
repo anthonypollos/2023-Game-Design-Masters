186e39ed379b6d2ad35bb8c9f66eb91fca9d7af5
jbthelt <jbarthelt@mail.bradley.edu>
2024-03-20 11:36:30 -0500

    Audio manager in town scene (for now)

--- Prototype1/Assets/Scripts/FMOD/AudioManager.cs
+++ Prototype1/Assets/Scripts/FMOD/AudioManager.cs
@@ -6,6 +6,22 @@ using FMOD.Studio;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager instance { get; private set;}
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("Found more than one Audio Manager in the scene");
+        }
+        instance = this;
+    }
+
+    public void PlayOneShot(EventReference sound, Vector3 worldPos)
+    {
+        RuntimeManager.PlayOneShot(sound, worldPos);
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
