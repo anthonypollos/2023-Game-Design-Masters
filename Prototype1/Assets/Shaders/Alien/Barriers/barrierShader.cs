5ae496042c1a6f5f9de89b81920370430ba12257
Wolfcl0ck <thewolfcl0ck@gmail.com>
2024-05-22 16:59:15 -0500

    progress on boss in test scene

--- Prototype1/Assets/Shaders/Alien/Barriers/barrierShader.cs
+++ Prototype1/Assets/Shaders/Alien/Barriers/barrierShader.cs
@@ -15,6 +15,9 @@ public class barrierShader : MonoBehaviour
     [SerializeField]
     private float growSpeed;
 
+    [SerializeField]
+    private bool loop;
+
     private bool increase = true;
 
     // Start is called before the first frame update
@@ -25,19 +28,24 @@ public class barrierShader : MonoBehaviour
         barrierMat.SetFloat("_BarrierGrow", grow);
     }
 
-    // Update is called once per frame
-    void Update()
+    // switched from update to fixedupdate because... yknow, tying VFX to framerate is a no-no
+    void FixedUpdate()
     {
         if (grow > barrierStop)
             increase = false;
         if (grow < barrierStart)
             increase = true;
-
-        if (increase)
-            grow = Mathf.Lerp(grow, barrierStop + 1, (growSpeed / 10) * Time.deltaTime);
+        if (loop)
+        {
+            if (increase)
+                grow = Mathf.Lerp(grow, barrierStop + 1, (growSpeed / 10) * Time.deltaTime);
+            else
+                grow = Mathf.Lerp(grow, barrierStart - 1, (growSpeed / 10) * Time.deltaTime);
+        }
         else
+        {
             grow = Mathf.Lerp(grow, barrierStart - 1, (growSpeed / 10) * Time.deltaTime);
-
+        }
         if(barrierMat != null)
             barrierMat.SetFloat("_BarrierGrow", grow);
     }
