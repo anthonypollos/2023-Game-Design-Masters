6f3dd796daa6d4d72f71acf7b78e6a5b1e1348f3
Wolfcl0ck <16748747+Wolfcl0ck@users.noreply.github.com>
2024-05-31 15:07:22 -0500

    restored old barrier behavior for final boss arena.

---
+++ Prototype1/Assets/Shaders/Alien/Barriers/barrierShaderOld.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barrierShaderOld : MonoBehaviour
+{
+    private Material barrierMat;
+
+    [SerializeField]
+    private float grow = 0;
+
+    [SerializeField]
+    private float barrierStart, barrierStop;
+
+    [SerializeField]
+    private float growSpeed;
+
+    [SerializeField]
+    private bool loop;
+
+    private bool increase = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        barrierMat = GetComponent<SkinnedMeshRenderer>().material;
+
+        barrierMat.SetFloat("_BarrierGrow", grow);
+    }
+
+    // switched from update to fixedupdate because... yknow, tying VFX to framerate is a no-no
+    void FixedUpdate()
+    {
+        if (grow > barrierStop)
+            increase = false;
+        if (grow < barrierStart)
+            increase = true;
+        if (loop)
+        {
+            if (increase)
+                grow = Mathf.Lerp(grow, barrierStop + 1, (growSpeed / 10) * Time.deltaTime);
+            else
+                grow = Mathf.Lerp(grow, barrierStart - 1, (growSpeed / 10) * Time.deltaTime);
+        }
+        else
+        {
+            grow = Mathf.Lerp(grow, barrierStart - 1, (growSpeed / 10) * Time.deltaTime);
+        }
+        if(barrierMat != null)
+            barrierMat.SetFloat("_BarrierGrow", grow);
+    }
+
+}
