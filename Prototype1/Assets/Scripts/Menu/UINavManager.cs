a6a0ba467794ade038be92ef73ba6fbe5398d3c3
avery1255 <amacke@mail.bradley.edu>
2024-06-19 11:46:22 -0500

    UI Navigation First Pass
    + pause menu, in-game options menu, level select, and journal all can be navigated/closed with designated menu/journal keys

---
+++ Prototype1/Assets/Scripts/Menu/UINavManager.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UINavManager : MonoBehaviour
+{
+    [Tooltip("Is this menu in a sub-menu?")]
+    public bool subMenuActive;
+    [Tooltip("Is this menu displaying a pop-up?")]
+    public bool popUpActive;
+    [Tooltip("Is this menu disabled?")]
+    public bool isClosed = true;
+
+    [Tooltip("This prefab's Main Menu; Should be immediately child of this object")]
+    [SerializeField] private GameObject mainMenu;
+
+    [Tooltip("Animator Component; Should be on the mainMenu GameObject")]
+    [SerializeField] private Animator anim;
+
+    [Tooltip("Is this menu in the Main Menu scene?")]
+    private bool isMainMenu = false;
+
+    /// <summary>
+    /// opens desired menu, sets timeScale to 0 and unlocks cursor
+    /// </summary>
+    public void OpenMainMenu()
+    {
+        isClosed = false;
+        subMenuActive = false;
+        popUpActive = false;
+
+        mainMenu.SetActive(true);
+
+        if(!isMainMenu)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Time.timeScale = 0;
+        }
+    }
+
+    /// <summary>
+    /// closes desired menu/returns to gameplay, sets timeScale back to 1 and locks cursor
+    /// </summary>
+    public void CloseMainMenu()
+    {
+        isClosed = true;
+        subMenuActive = false;
+        popUpActive = false;
+
+        mainMenu.SetActive(false);
+
+        if(!isMainMenu)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Time.timeScale = 1;
+        }
+    }
+
+    /// <summary>
+    /// Closes any sub-menus or pop-ups & returns to main menu
+    /// </summary>
+    /// <param name="animTrigger">anim trigger to return to main menu; likely called "Main"</param>
+    public void ReturnToMain(string animTrigger)
+    {
+        subMenuActive = false;
+        popUpActive = false;
+
+        anim.SetTrigger(animTrigger);
+    }
+
+    /// <summary>
+    /// Opens sub-menu specified by animTrigger variable
+    /// </summary>
+    /// <param name="animTrigger">anim Trigger related to desired sub-menu</param>
+    public void OpenSubMenu(string animTrigger)
+    {
+        subMenuActive = true;
+
+        anim.SetTrigger(animTrigger);
+    }
+
+    /// <summary>
+    /// Closes active sub-menu, returns to main menu
+    /// </summary>
+    /// <param name="animTrigger">anim trigger to return to main menu; likely called "Main"</param>
+    public void CloseSubMenu(string animTrigger)
+    {
+        subMenuActive = false;
+
+        anim.SetTrigger(animTrigger);
+    }
+
+    /// <summary>
+    /// Opens pop-up window
+    /// </summary>
+    /// <param name="animTrigger">anim trigger to open pop-up window; likely called "PopUp"</param>
+    public void OpenPopUp(string animTrigger)
+    {
+        popUpActive = true;
+
+        anim.SetTrigger(animTrigger);
+    }
+
+    /// <summary>
+    /// Closes active pop-up window, returns to main menu
+    /// </summary>
+    /// <param name="animTrigger">anim trigger to return to main menu; likely called "Main"</param>
+    public void ClosePopUp(string animTrigger)
+    {
+        popUpActive = false;
+
+        anim.SetTrigger(animTrigger);
+    }
+}
