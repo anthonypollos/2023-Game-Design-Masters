6be41e3a18fc47823e25531aa8cb121de634b5f8
jbthelt <jbarthelt@mail.bradley.edu>
2024-07-12 11:41:28 -0500

    Regrow plant noises, set up ritual circle ambience

--- Prototype1/Assets/Scripts/World/FallingGroundSound.cs
+++ Prototype1/Assets/Scripts/World/FallingGroundSound.cs
@@ -11,7 +11,7 @@ public class FallingGroundSound : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PlaySound();
+        
         //asource.Play();
     }
 
