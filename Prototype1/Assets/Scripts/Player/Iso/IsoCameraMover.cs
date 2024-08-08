8ef37124f839dfef16e028f32f912f1cc032ee91
Wolfcl0ck <thewolfcl0ck@gmail.com>
2024-08-08 12:02:50 -0500

    IsoCameraMover moves at half-speed towards target.

--- Prototype1/Assets/Scripts/Player/Iso/IsoCameraMover.cs
+++ Prototype1/Assets/Scripts/Player/Iso/IsoCameraMover.cs
@@ -7,8 +7,10 @@ public class IsoCameraMover : MonoBehaviour
 
     private GameObject mainCamera;
     [Tooltip("The amount of time the camera will hold on this object after it's spawned.")][SerializeField] private float holdTime = 1f;
+    [Tooltip("The smoothing (movement) rate of the camera as it moves to the target.")] [SerializeField] private float cameraMoveRate = 5f;
     [Tooltip("ENEMY ONLY!\nMove the camera on enemy aggro.")] [SerializeField] private bool cameraOnAggro = false;
 
+    private float cameraSmoothing;
 
     // Start is called before the first frame update
     void Start()
@@ -25,14 +27,29 @@ public class IsoCameraMover : MonoBehaviour
     private IEnumerator GoBackToPlayer(float time)
     {
         yield return new WaitForSeconds(time);
+        //Set the camera's smoothing rate back to its original
+        mainCamera.GetComponent<IsoCamera>()._smoothing = cameraSmoothing;
+
+        //If this check is true, it means that the camera smoothing rate *after* resetting it is still the same as the slowed rate from here.
+        //This should NOT happen and means that cameraSmoothing was somehow set to be equal to cameraMoveRate at some point.
+        //If this happens, we assume that the correct number is 10f (our level default) and set it to this.
+        if (mainCamera.GetComponent<IsoCamera>()._smoothing == cameraMoveRate)
+        {
+            cameraSmoothing = 10f; //superfluous but better safe than sorry.
+            mainCamera.GetComponent<IsoCamera>()._smoothing = 10f;
+        }
         mainCamera.GetComponent<IsoCamera>().RevertTarget();
     }
 
     public void StealCamera()
     {
-        //3. If the target is the player, set the target to this
+        //Set the private float here to whatever the default smoothing value is on the camera
+        cameraSmoothing = mainCamera.GetComponent<IsoCamera>()._smoothing;
+        //set the camera's smoothing to our set value.
+        mainCamera.GetComponent<IsoCamera>()._smoothing = cameraMoveRate;
+        //If the target is the player, set the target to this
         mainCamera.GetComponent<IsoCamera>().ChangeTarget(gameObject);
-        //4. After we've set the camera target to this, wait for holdtime and then revert to the player
+        //After we've set the camera target to this, wait for holdtime and then revert to the player
         StartCoroutine(GoBackToPlayer(holdTime));
     }
 
