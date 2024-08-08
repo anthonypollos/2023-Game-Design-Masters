453838ee4eea8c2090f00f79959c5ace52a1a98f
avery1255 <amacke@mail.bradley.edu>
2024-08-08 14:40:43 -0500

    volume sliders play sounds when adjusting volume

--- Prototype1/Assets/Scripts/Menu/Options/TestAudioLevel.cs
+++ Prototype1/Assets/Scripts/Menu/Options/TestAudioLevel.cs
@@ -3,32 +3,66 @@
  */
 using UnityEngine;
 using UnityEngine.EventSystems;
+using FMODUnity;
 
-public class TestAudioLevel : MonoBehaviour, IPointerUpHandler
+public class TestAudioLevel : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
 {
     /// <summary>
     /// Reference to AudioSource component
     /// </summary>
-    private AudioSource audioSource;
+    //private AudioSource audioSource;
 
-    [SerializeField] [Tooltip("Audio clip to play after changing volume slider")] private AudioClip testClip;
+    [SerializeField] private StudioEventEmitter eventEmitter;
+
+    [SerializeField] private StudioEventEmitter[] emitters;
+
+    //[SerializeField] [Tooltip("Audio clip to play after changing volume slider")] private AudioClip testClip;
 
     private void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        //audioSource = GetComponent<AudioSource>();
     }
 
     /// <summary>
     /// Plays audio clip when mouse releases slider
     /// </summary>
     /// <param name="eventData"></param>
-    public void OnPointerUp(PointerEventData eventData)
+    public void OnPointerDown(PointerEventData eventData)
     {
-        PlaySound();
+        //PlaySound();
+        if (eventEmitter != null)
+        {
+            if (eventEmitter.IsPlaying())
+                eventEmitter.Stop();
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        eventEmitter.enabled = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        eventEmitter.enabled = false;
     }
 
     public void PlaySound()
     {
+        foreach(StudioEventEmitter emitter in emitters)
+        {
+            emitter.Stop();
+        }
+
+        if(eventEmitter != null)
+        {
+            if (eventEmitter.IsPlaying())
+                eventEmitter.Stop();
+
+            eventEmitter.Play();
+        }
+
+        /*
         if (audioSource != null)
         {
             if (audioSource.isPlaying)
@@ -36,6 +70,7 @@ public class TestAudioLevel : MonoBehaviour, IPointerUpHandler
 
             audioSource.PlayOneShot(testClip);
         }
+        */
     }
 
 }
